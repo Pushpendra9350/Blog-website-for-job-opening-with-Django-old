1f9450a20395d594ecd5df16256b2b50f06cc4ce
Pushpendra Kumar <pushpendra9350@gmail.com>
2021-07-16 23:23:27 +0530

    Posts are comming from database

--- blogWebsite/blogapp/models.py
+++ blogWebsite/blogapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.utils import timezone 
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    """
+    # Here we are using only 100 length content for the title of the blog post
+    # TextField is used for the lengthy text
+    # DateTimeField(auto_now=True)  when we update next time our post then it will update to the current date every time 
+    # DateTimeField(auto_now_add=True) it set dateand time to the time when it object is created but we can not update it.
+    # DateTimeField(default=timezone.now) now we can also update the date time of the post when we want 
+    # User and Post will be in the one to many relation ship when we delete a user the all posts will be delete
+    """
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User,on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- blogWebsite/blogapp/views.py
+++ blogWebsite/blogapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
 
 posts=[
@@ -18,7 +19,7 @@ posts=[
 
 def home(request):
     context = {
-        "posts":posts
+        "posts":Post.objects.all()
     }
     return render(request, 'blogapp/home.html',context,status=201,)
 
