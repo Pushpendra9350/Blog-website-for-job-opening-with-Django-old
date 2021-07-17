53d476da29610f4c6f4ecb445caeeaceebd45785
Pushpendra Kumar <pushpendra9350@gmail.com>
2021-07-17 13:52:44 +0530

    With user registration

--- blogWebsite/blogWebsite/urls.py
+++ blogWebsite/blogWebsite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from userapp import views as user_app_view
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("register/",user_app_view.register),
     path('',include("blogapp.urls"))
 ]
