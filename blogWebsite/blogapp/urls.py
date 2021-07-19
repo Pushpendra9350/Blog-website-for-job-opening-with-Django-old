2e71fbc486d5d7510d19a635712eb5668cc0bd5a
Pushpendra Kumar <pushpendra9350@gmail.com>
2021-07-19 12:40:12 +0530

    Final with out documentation

--- blogWebsite/blogapp/urls.py
+++ blogWebsite/blogapp/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
 from . import views
+from .views import(
+     PostListView, 
+     PostDetailsView, 
+     PostCreateView, 
+     PostUpdateView,
+     PostDeleteView,
+     PostListUserView
+)
 
 
 urlpatterns = [
     # Why we are writing this name here as blog-home
     # it to clearify that we are talking about blog home not other apps home
-    path('', views.home, name="blog-home"),
+    path('', PostListView.as_view(), name="blog-home"),
+    path('post/<int:pk>/', PostDetailsView.as_view(), name="post-detail"),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name="post-update"),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name="post-delete"),
+    path('post/user/<int:author>', PostListUserView.as_view(), name="post-delete"),
+    path('post/new/', PostCreateView.as_view(), name="post-create"),
     path('about/', views.about, name="blog-about")
 ]
--- blogWebsite/blogapp/views.py
+++ blogWebsite/blogapp/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from django.core.paginator import Paginator
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import( 
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView,
+    DeleteView
+    )
 # Create your views here.
 
 posts=[
@@ -22,6 +31,52 @@ def home(request):
         "posts":Post.objects.all()
     }
     return render(request, 'blogapp/home.html',context,status=201,)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blogapp/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 4
+
+class PostDetailsView(DetailView):
+    model = Post
+    
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title","content"]
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title","content"]
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = "/"
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostListUserView(ListView):
+    model = Post
+    template_name = 'blogapp/post_per_user.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 2
 
 def about(request):
     return render(request, 'blogapp/about.html')
